85c5cf01bac747d983eb167f5e67d5aabe85ee16
Omprakash <omprakasht5689@gmail.com>
2025-10-16 13:30:36 +0530

    Time: 0 ms (100%), Space: 41.6 MB (38.87%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String[] findWords(String[] words) {
+                String row1 = "qwertyuiop";
+        String row2 = "asdfghjkl";
+        String row3 = "zxcvbnm";
+        
+        List<String> result = new ArrayList<>();
+        
+        for (String word : words) {
+            String lowerWord = word.toLowerCase();
+            
+            // Determine which row the first letter belongs to
+            String row;
+            if (row1.indexOf(lowerWord.charAt(0)) != -1) {
+                row = row1;
+            } else if (row2.indexOf(lowerWord.charAt(0)) != -1) {
+                row = row2;
+            } else {
+                row = row3;
+            }
+            
+            // Check if all characters belong to the same row
+            boolean valid = true;
+            for (char c : lowerWord.toCharArray()) {
+                if (row.indexOf(c) == -1) {
+                    valid = false;
+                    break;
+                }
+            }
+            
+            if (valid) {
+                result.add(word);
+            }
+        }
+        
+        return result.toArray(new String[0]);
+
+    }
+}
